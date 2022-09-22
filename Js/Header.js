59dbd069cc066900dc4bbf21a078a1e56a4a1947
AKiumarsi <a.kiumarsi.ir@gmail.com>
2022-09-23 00:28:01 +0330

    adding task bar icon for mobile mod and start icon

--- Js/Header.js
+++ Js/Header.js
@@ -2,7 +2,7 @@
 h1 = 20"-"(2)
 h2 = 15"-"(2)*/
 
-// h1-----------------------------onClickMenu-----------------------------
+// h1-----------------------------onClickMenu-desktop & tablet-----------------------------
 const menuIcon = document.getElementById("menuIcon");
 const body = document.getElementById("body");
 const header = document.getElementById("header");
@@ -81,6 +81,7 @@ menuIcon.addEventListener("click", () => {
   }
 });
 
+// h2---------------dark-backgraound-whenNav-expanded--------------h2
 menuDarkBack.addEventListener("click", () => {
   nav.classList.remove("nav-width-tablet");
   for (let x = 0; x < navlinkTextStyle.length; x++) {
@@ -96,4 +97,60 @@ function navbarHeight() {
   const navbarNav = document.getElementById("navbarNav");
   let heightnav = window.innerHeight;
   navbarNav.style.height = heightnav + "px";
+}
+
+// h1-----------------------------menu-icon-mobile----------------------------
+const menuIconMobile = document.getElementById("menuIconMobile");
+
+menuIconMobile.addEventListener("click", () => {
+  const visibilityMobile = menuIconMobile.getAttribute("data-visible");
+  const menuIconItem1Mobile = document.getElementById("menuIconItem1Mobile");
+  const menuIconItem2Mobile = document.getElementById("menuIconItem2Mobile");
+  const menuIconItem3Mobile = document.getElementById("menuIconItem3Mobile");
+  const menuIconItem4Mobile = document.getElementById("menuIconItem4Mobile");
+  const root = document.querySelector(":root");
+
+  if (visibilityMobile === "false") {
+    // منو فالس و بسته می باشد با کلیک باز می شود موبایل
+    // h2---------------menuiconanimate--------------h2
+    menuIconItem1Mobile.classList.add("menu-icon-item1-mobile-animate");
+    menuIconItem2Mobile.classList.add("menu-icon-item2-mobile-animate");
+    menuIconItem3Mobile.classList.add("menu-icon-item3-mobile-animate");
+    menuIconItem4Mobile.classList.add("menu-icon-item4-mobile-animate");
+    menuIconItem1Mobile.classList.remove("menu-icon-item1-mobile-animate2");
+    menuIconItem2Mobile.classList.remove("menu-icon-item2-mobile-animate2");
+    menuIconItem3Mobile.classList.remove("menu-icon-item3-mobile-animate2");
+    menuIconItem4Mobile.classList.remove("menu-icon-item4-mobile-animate2");
+
+    // h1-----------------------------navHeight-for-mobile-----------------------------
+    nav.classList.add("mobile-nav-height");
+
+    menuIconMobile.setAttribute("data-visible", true);
+  } else if (visibilityMobile === "true") {
+    // منو باز بوده و با کلیک بسته می شود موبایل
+
+    // h2---------------menuiconanimate--------------h2
+    menuIconItem1Mobile.classList.remove("menu-icon-item1-mobile-animate");
+    menuIconItem2Mobile.classList.remove("menu-icon-item2-mobile-animate");
+    menuIconItem3Mobile.classList.remove("menu-icon-item3-mobile-animate");
+    menuIconItem4Mobile.classList.remove("menu-icon-item4-mobile-animate");
+    menuIconItem1Mobile.classList.add("menu-icon-item1-mobile-animate2");
+    menuIconItem2Mobile.classList.add("menu-icon-item2-mobile-animate2");
+    menuIconItem3Mobile.classList.add("menu-icon-item3-mobile-animate2");
+    menuIconItem4Mobile.classList.add("menu-icon-item4-mobile-animate2");
+
+    // h1-----------------------------navHeight-for-mobile-----------------------------
+    nav.classList.remove("mobile-nav-height");
+
+    menuIconMobile.setAttribute("data-visible", false);
+  }
+});
+
+// h1-----------------------------navHeight-for-mobile-----------------------------
+window.addEventListener("resize", mobileNavHeightF);
+mobileNavHeightF();
+function mobileNavHeightF() {
+  const root = document.querySelector(":root");
+  let mobileNavHeight = window.innerHeight - 20 + "px";
+  root.style.setProperty("--mobile-nav-height", mobileNavHeight);
 }
