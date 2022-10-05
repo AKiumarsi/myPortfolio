bfebbfd7a9f2091d62186d67804749c65c05b47a
AKiumarsi <a.kiumarsi.ir@gmail.com>
2022-10-05 23:46:19 +0330

    create and start fot main.js

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+const homeSecParg = document.getElementById("homeSecParg");
+
+homeSecPargF();
+window.addEventListener("resize", homeSecPargF);
+function homeSecPargF() {
+  const homeSecPargHei = window.getComputedStyle(homeSecParg);
+  const bb = homeSecPargHei.getPropertyValue("height");
+
+
+  if ((window.innerWidth > 576) & (window.innerHeight > "465")) {
+    homeSecParg.style.height = window.innerHeight - 151 + "px";
+    console.log(innerHeight)
+  }
+}
