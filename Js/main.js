8504eb3504c554a0688f6466c9c90c00ee6b5664
AKiumarsi <a.kiumarsi.ir@gmail.com>
2022-10-07 15:01:20 +0330

    create linktip for link Icons

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,25 @@
+// این برای اولین سکشن یعنی هومه که ارتفاعش و مشخص می کنه
 const homeSecParg = document.getElementById("homeSecParg");
-
 homeSecPargF();
 window.addEventListener("resize", homeSecPargF);
 function homeSecPargF() {
   const homeSecPargHei = window.getComputedStyle(homeSecParg);
   const bb = homeSecPargHei.getPropertyValue("height");
 
-
-  if ((window.innerWidth > 576) & (window.innerHeight > "465")) {
+  if (
+    (window.innerWidth > 576) &
+    (window.innerHeight > "465") &
+    (window.innerHeight < 2001) &
+    (window.innerWidth < 4001)
+  ) {
     homeSecParg.style.height = window.innerHeight - 151 + "px";
   }
 }
+
+// این برای حذف راهنمای اسکرول با اسکرول کردنه
+window.addEventListener("scroll", () => {
+  const angleDownScroll = document.getElementById("angleDownScroll");
+  if (scrollY > 200) {
+    angleDownScroll.style.display = "none";
+  }
+});
